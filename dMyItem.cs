ecd7502686f932e8674133b12180f94fefb684a0
Rabiu Oluwaseun <rabiuoluwaseundmd@gmail.com>
2022-03-12 18:36:30 +0100

    Update dMyItem.cs

--- dMyItem.cs
+++ dMyItem.cs
@@ -7,7 +7,6 @@ public class dMyItem
     public string name;
     public int id;
     public string description;
-    // protected is used when you want only the classes that inherit from this base class therefore its private but accessible by child classes of dMyItem 
     public bool isStackable;
     public Sprite icon;
     // Constructor 
